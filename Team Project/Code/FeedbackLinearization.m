dfaf8fae39630ff0abc3989090b2be04f511380c
George Krommydas <geokrom98@gmail.com>
2024-01-23 17:06:26 +0200

    Delete Team Project/Code directory

--- Team Project/Code/FeedbackLinearization.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = FeedbackLinearizition(inputArg1,inputArg2)
-%FEEDBACKLINEARIZITION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
