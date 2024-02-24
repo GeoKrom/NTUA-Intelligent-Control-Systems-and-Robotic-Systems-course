19fbdd6b3ebd9020dd11239551bae501fad8b166
George Krommydas <geokrom98@gmail.com>
2024-02-24 13:26:18 +0200

    Add files via upload

--- Team Project/Arduino Code/MRAC_Control_Loop/MRAC_Control_Loop.ino
+++ Team Project/Arduino Code/MRAC_Control_Loop/MRAC_Control_Loop.ino
@@ -21,12 +21,18 @@ const int LOADCELL_SCK_PIN_bot = 8;
 const float scale_bot_value = 142479.4/199; // divide with [gr]
 float h2_cur;
 float h2_prev;
+
 HX711 scale_bot;
 
 // Pin definitions for water pump
 const int ENA = 10; // PWM
 const int MOTOR1_PIN1 = 2; // 
 const int MOTOR1_PIN2 = 3;
+
+// time constanst
+float time_prev = 0.0;
+float time_cur = 0.0;
+
 
 // Define System Parameters
 #define a1 PI *pow(2 * pow(10, -3), 2)
@@ -39,8 +45,8 @@ const int MOTOR1_PIN2 = 3;
 #define rho 1*pow(10,3) // [kg/m^2]
 
 // Define Adaptive Feedback Linearization Controller Parameters
-#define gammax 500
-#define gammar 100
+#define gammax 500000
+#define gammar 100000
 #define dt 0.01 // in [sec]
 
 const float theta1 = ((a1 / A) * sqrt(2 * g));
@@ -48,10 +54,10 @@ const float theta2 = ((a2 / A) * sqrt(2 * g));
 const float b = Km / A;
 //const float h_max = pow(10, -3) / A;
 const float h_max = 0.06;
-const float h1_e = h_max / 2;
+const float h1_e = h_max / 4;
 const float h2_e = pow(theta2 / theta1, 2) * h1_e;
 const float u_e = (theta1 / b) * sqrt(h1_e);
-const float a2m = 0.05;
+const float a2m = 4;
 const float a1m = 2 * sqrt(a2m);
 const float K1 = a2m;
 const float K2 = 2*sqrt(a2m);
@@ -60,6 +66,7 @@ float system_state[] = {0, 0, 0, 0, 0, 0, 0};
 
 // Global Variable
 bool enabled = false;
+char userInput;
 
 void setup() {
   Serial.begin(9600);
@@ -83,45 +90,71 @@ void setup() {
   digitalWrite(MOTOR1_PIN2, LOW);
   
   // Initial state
-  system_state[0] = h1_e;
-  system_state[1] = h2_e;
-  system_state[4] = -20;
-  system_state[5] = -200;
-  system_state[6] = 15;
+  // system_state[0] = h1_e;
+  // system_state[1] = h2_e;
+  system_state[4] = -40;
+  system_state[5] = -12;
+  system_state[6] = 24;
 
 }
 
 void loop() {
-
- if (!enabled) {
-    initialization();
-  }
-  // Control Loop
-  if (enabled) {
-    h2_prev = system_state[1];
-    readTopLoadCellMeasurement();
-    readBotLoadCellMeasurement();
-    h2_cur = system_state[1];
-    float h2_dot = (h2_cur - h2_prev)/dt;
-    float pump_value = MRAC(h2_dot);
-    // Transform to [V]
-    int volt_pump_value =
-        map(pump_value, V_LOW, V_HIGH, V_BINARY_LOW, V_BINARY_HIGH);
-    sendPump(volt_pump_value);
-    delay(dt * 1000);
+    if(Serial.available()> 0){ 
+        // Serial.print('1');
+        userInput = Serial.read();     
+        if(userInput == 'g'){  
+    
+  if (!enabled) {
+      initialization();
+    }
+    // Control Loop
+    if (enabled) {
+      h2_prev = system_state[1];
+      readTopLoadCellMeasurement();
+      readBotLoadCellMeasurement();
+      h2_cur = system_state[1];
+      time_cur = millis();
+      float h2_dot = (h2_cur - h2_prev)/(time_cur - time_prev);
+      float pump_value = MRAC(h2_dot);
+      if (pump_value<0){pump_value = 0;}
+      if (pump_value>5){pump_value = 3;}
+      // Transform to [V]
+      int volt_pump_value =
+          map(pump_value, V_LOW, V_HIGH, V_BINARY_LOW, V_BINARY_HIGH);
+      
+      sendPump(volt_pump_value);
+      delay(dt * 1000);
+      h2_prev = system_state[1];
+      time_prev = millis();
+      Serial.print(system_state[1]*1000);
+        Serial.print(",");
+        Serial.print((system_state[2]+h2_e)*1000);
+        Serial.print(",");
+        Serial.print(millis());
+        Serial.print(",");
+        Serial.print("volt");
+        Serial.print(",");
+        Serial.print(volt_pump_value);
+        Serial.print(",");
+      }
+    }
+    else if (userInput == 'c'){
+      sendPump(0);
+    }
   }
 }
-
 void initialization() {
 
   float h1 = 0;
   float h2 = 0;
   sendPump(90); // MAX 255 
-  // while (h1 < h1_e || h2 < h2_e) {
-  while (true){
+  while (h1 < h1_e || h2 < h2_e) {
+  // while (true){
     h1 = readTopLoadCellMeasurement();
     h2 = readBotLoadCellMeasurement();
   }
+  system_state[0] = h1;
+  system_state[1] = h2;
   enabled = true;
   Serial.println("Target Reached");
 
@@ -133,10 +166,14 @@ void initialization() {
 float MRAC(float h2_dot) {
 
   float r = ref() - h2_e;
-  BLA::Matrix<2, 2> Am = {0, 1, -a2m, -a1m};
-  BLA::Matrix<2, 1> Bm = {0, b};
-  BLA::Matrix<2, 2> P = {5.6461, 10, 10, 23.4787};
-  
+  // BLA::Matrix<2, 2> Am = {0, 1, -a2m, -a1m};
+  // BLA::Matrix<2, 1> Bm = {0, b};
+  // BLA::Matrix<2, 2> P = {5.6461, 10, 10, 23.4787};
+  float P11 = 5.6461;
+  float P12 = 10;
+  float P22 = 23.4787;
+
+  // Decompose state variables
   // Decompose state variables
   float h1 = system_state[0];
   float h2 = system_state[1];
@@ -145,54 +182,84 @@ float MRAC(float h2_dot) {
   float Kx1_est = system_state[4];
   float Kx2_est = system_state[5];
   float Kr_est = system_state[6];
-
-  BLA::Matrix<1, 2> Kx_est = {Kx1_est, Kx2_est};
+  
+  float dxm0 = xm_dot;
+  float dxm1 = -a2m*xm - a1m*xm_dot + a2m*r ;
+  
   // Apply MRAC Controller
-  BLA::Matrix<2, 1> x_m = {xm, xm_dot};
-  BLA::Matrix<2, 1> dxm = Am * x_m + Bm * r;
-  BLA::Matrix<2, 1> B = {0, 1};
-  BLA::Matrix<2, 1> x = {h2 - h2_e, h2_dot};
-  BLA::Matrix<2, 1> e = x - x_m;
-  BLA::Matrix<1, 2> e_transpose = {e(0),e(1)};
-  BLA::Matrix<2, 2> Gamma_x = {gammax,0,0,gammax};
-  BLA::Matrix<1, 1> gamma_r = {gammar};
-  BLA::Matrix<2, 1> dKx = -Gamma_x*x*e_transpose*P*B;
-  BLA::Matrix<1, 1> dKr = -gamma_r*r*e_transpose*P*B;
+  float e0 = h2 - h2_e - xm;
+  float e1 = h2_dot - xm_dot;
+  float dKx0 = -gammax*(h2-h2_e)*(e0*P12 + e1*P22);
+  float dKx1 = -gammax*h2_dot*(e0*P12 + e1*P22);
+  float dKr = -gammar*r*(e0*P12 + e1*P22);
   
   // Projection
-  if(Kr_est <= 0 && dKr(0) < 0){
-    dKr(0) = 0;
+  if(Kr_est <= 0 && dKr < 0){
+    dKr = 0;
   }
-  if(Kr_est >= 1000 && dKr(0) > 0){
-    dKr(0) = 0;
+  if(Kr_est >= 1000 && dKr > 0){
+    dKr = 0;
+  }
+  if(Kx1_est >= 0 && dKx0 > 0){
+    dKx0 = 0;
+  }
+  if(Kx1_est <= -1000 && dKx0 < 0){
+    dKx0 = 0;
   }
 
-  if(Kx1_est >= 0 && dKx(0) > 0){
-    dKx(0) = 0;
+  if(Kx2_est >= 0 && dKx1 > 0){
+    dKx1 = 0;
   }
-  if(Kx1_est <= -1000 && dKx(0) < 0){
-    dKx(0) = 0;
-  }
-
-  if(Kx2_est >= 0 && dKx(0) > 0){
-    dKx(0) = 0;
-  }
-  if(Kx2_est <= -1000 && dKx(0) < 0){
-    dKx(0) = 0;
+  if(Kx2_est <= -1000 && dKx1 < 0){
+    dKx1 = 0;
   }
 
   
-  BLA::Matrix<1, 1> u_c = Kx_est*x + Kr_est*r;
-  float u = u_c(0);
+  float u = Kx1_est*(h2-h2_e) + Kx2_est*h2_dot + Kr_est*r + 1.76;
+
   // Compute new state
   system_state[0] = h1;
   system_state[1] = h2;
-  system_state[2] = xm + dt * dxm(0);
-  system_state[3] = xm_dot + dt * dxm(1);
-  system_state[4] = Kx1_est + dKx(0) * dt;
-  system_state[5] = Kx2_est + dKx(1) * dt;
-  system_state[6] = Kr_est + dKr(0) * dt;
+  system_state[2] = xm + dt * dxm0;
+  system_state[3] = xm_dot + dt * dxm1;
+  system_state[4] = Kx1_est + dKx0 * dt;
+  system_state[5] = Kx2_est + dKx1 * dt;
+  system_state[6] = Kr_est + dKr * dt;
 
+Serial.print("law");
+  Serial.print(",");  
+  Serial.print(u);
+  Serial.print(","); 
+  Serial.print("error");
+  Serial.print(",");
+  Serial.print(e0*1000);
+  Serial.print(",");
+  Serial.print(e1*1000);
+  Serial.print(",");
+  Serial.print("state: ");
+  Serial.print(",");
+  // Serial.print(system_state[0]*1000);
+  // Serial.print(",");
+  // Serial.print(system_state[1]*1000);
+  // Serial.print(",");
+  // Serial.print(system_state[2]*1000);
+  // Serial.print(",");
+  // Serial.print(system_state[3]*1000);
+  // Serial.print(",");
+  Serial.print(system_state[4]*1000);
+  Serial.print(",");
+  Serial.print(system_state[5]*1000);
+  Serial.print(",");
+  Serial.print(system_state[6]*1000);
+  Serial.print(",");
+  Serial.print(system_state[6]*1000);
+  Serial.print(",");
+  Serial.print(dKx0*1000);
+  Serial.print(",");
+  Serial.print(dKx1*1000);
+  Serial.print(",");
+  Serial.println(dKr*1000);
+  
   return u;
 }
 
